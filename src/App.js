2ae76ac18c3c67576800bc087b3246a0740e9238
Daniele Guido <gui.daniele@gmail.com>
2022-11-14 15:56:48 +0100

    Separate background and add static page in App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import "./App.css";
-import Home from "./pages/Home";
-import MenuFullScreen from "./components/menu/MenuFullScreen";
-import { useCurrentWindowDimensions } from "./hooks/viewport";
+import './App.css'
+import Home from './pages/Home'
+import MenuFullScreen from './components/menu/MenuFullScreen'
+import { useCurrentWindowDimensions } from './hooks/viewport'
+import Header from './components/Header'
+import Page from './pages/Page'
+import Background from './components/Background'
 
 function App({ isMobile }) {
-  const { width, height } = useCurrentWindowDimensions(isMobile);
+  const { width, height } = useCurrentWindowDimensions(isMobile)
   return (
-    <div className="App">
+    <div className='App'>
+      <Background></Background>
       <main>
         <MenuFullScreen availableWidth={width} availableHeight={height} />
-        <Home availableWidth={width} availableHeight={height}></Home>
+        <Header></Header>
+        <Page></Page>
+
+        <Home
+          hideWhenPathIs={['/cfp']}
+          availableWidth={width}
+          availableHeight={height}
+        ></Home>
       </main>
     </div>
-  );
+  )
 }
 
-export default App;
+console.info(
+  'version',
+  process.env.REACT_APP_GIT_TAG,
+  process.env.REACT_APP_GIT_BRANCH,
+  `\nhttps://github.com/C2DH/dhd2023/commit/${process.env.REACT_APP_GIT_REVISION}`,
+  '\n -page CFP:',
+  process.env.REACT_APP_PAGES_CFP_URL
+)
+
+export default App
---
+++ src/components/Background.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom'
+import { useMenuStore } from '../store'
+import './Background.css'
+
+const Background = () => {
+  const currentStepIndex = useMenuStore((state) => state.currentStepIndex)
+  const location = useLocation()
+
+  let classColor = 'bg-white'
+
+  if (location.pathname === '/cfp') {
+    classColor = 'bg-primary'
+  } else {
+    if ([0, 2, 4].includes(currentStepIndex)) {
+      classColor = 'bg-light'
+    }
+    if ([1, 3].includes(currentStepIndex)) {
+      classColor = 'bg-dark'
+    }
+  }
+
+  console.debug('[Background] classColor:', classColor)
+
+  return (
+    <div
+      className={`Background position-fixed w-100 h-100 ${classColor}`}
+      style={{ backgroundColor: 'red' }}
+    >
+      {currentStepIndex}
+    </div>
+  )
+}
+
+export default Background
