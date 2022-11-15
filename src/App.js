bdb9d7624ecf5ed9779be75ad8229798813b0fba
Daniele Guido <gui.daniele@gmail.com>
2022-11-15 11:52:47 +0100

    do not retry axios requests if not needed

--- src/App.js
+++ src/App.js
@@ -5,23 +5,40 @@ import { useCurrentWindowDimensions } from './hooks/viewport'
 import Header from './components/Header'
 import Page from './pages/Page'
 import Background from './components/Background'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// Create a client
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: Infinity,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchInterval: false,
+      refetchIntervalInBackground: false,
+      refetchOnMount: false,
+      staleTime: Infinity,
+      retry: false,
+      suspense: false,
+      keepPreviousData: true,
+    },
+  },
+})
 
 function App({ isMobile }) {
   const { width, height } = useCurrentWindowDimensions(isMobile)
   return (
-    <div className='App'>
-      <Background></Background>
-      <main>
-        <MenuFullScreen availableWidth={width} availableHeight={height} />
-        <Header></Header>
-        <Page></Page>
+    <div className="App">
+      <QueryClientProvider client={queryClient}>
+        <Background></Background>
+        <main>
+          <MenuFullScreen availableWidth={width} availableHeight={height} />
+          <Header></Header>
+          <Page></Page>
 
-        <Home
-          hideWhenPathIs={['/cfp']}
-          availableWidth={width}
-          availableHeight={height}
-        ></Home>
-      </main>
+          <Home hideWhenPathIs={['/cfp']} availableWidth={width} availableHeight={height}></Home>
+        </main>
+      </QueryClientProvider>
     </div>
   )
 }
@@ -32,7 +49,7 @@ console.info(
   process.env.REACT_APP_GIT_BRANCH,
   `\nhttps://github.com/C2DH/dhd2023/commit/${process.env.REACT_APP_GIT_REVISION}`,
   '\n -page CFP:',
-  process.env.REACT_APP_PAGES_CFP_URL
+  process.env.REACT_APP_PAGES_CFP_URL,
 )
 
 export default App
