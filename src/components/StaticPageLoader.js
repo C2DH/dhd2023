2e998b91e7f4a5ea41f72ae64797e071de9a2b26
Daniele Guido <gui.daniele@gmail.com>
2022-11-15 11:53:00 +0100

    print out received data (if any)

---
+++ src/components/StaticPageLoader.js
@@ -0,0 +1,94 @@
+import React, { useEffect } from 'react'
+import ErrorViewer from './ErrorViewer'
+import { useSpring, a, config } from '@react-spring/web'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+
+const StaticPageLoader = ({ url, delay = 0, Component, fakeData, raw = false, ...rest }) => {
+  const [animatedLine, apiAnimatedLine] = useSpring(() => ({
+    width: 0,
+    opacity: 1,
+    config: config.slow,
+  }))
+
+  const { data, error, status } = useQuery([url], () =>
+    axios.get(url, {
+      baseUrl: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROXY : '',
+    }),
+  )
+  // const { data, error, status } = useGetJSON({
+  //   url,
+  //   delay,
+  //   raw,
+  //   onDownloadProgress: (e) => {
+  //     console.debug('[StaticPageLoader] onDownloadProgress url', url, e.total, e.loaded)
+  //     if (!e.total && e.loaded > 0) {
+  //       apiAnimatedLine.start({ width: Math.min(100, (100 * e.loaded) / 23810103) })
+  //     } else if (e.total && e.loaded) {
+  //       if (e.loaded < e.total) {
+  //         apiAnimatedLine.start({ width: (100 * e.loaded) / e.total })
+  //       }
+  //     }
+  //   },
+  // })
+
+  useEffect(() => {
+    if (status === 'loading') {
+      apiAnimatedLine.start({
+        width: 10,
+        opacity: 1,
+      })
+    } else if (status === 'success') {
+      apiAnimatedLine.start({
+        width: 100,
+        opacity: 0,
+      })
+    } else if (status === 'error') {
+      apiAnimatedLine.start({
+        width: 0,
+        opacity: 0,
+      })
+    }
+  }, [status])
+  console.debug('[StaticPageLoader] status:', status, url)
+  return (
+    <>
+      <div
+        className="position-fixed w-100"
+        style={{
+          top: 0,
+          left: 0,
+          zIndex: 10,
+        }}
+      >
+        <a.div
+          className="NotebookViewer_loading position-absolute"
+          style={{
+            opacity: animatedLine.opacity,
+            width: animatedLine.width.to((x) => `${x}%`),
+          }}
+        />
+        <a.span
+          className="NotebookViewer_loadingPercentage monospace"
+          style={{
+            opacity: animatedLine.opacity,
+          }}
+        >
+          {animatedLine.width.to((x) => `${Math.round(x * 10000) / 10000}%`)}
+        </a.span>
+      </div>
+
+      {status === 'error' && <ErrorViewer error={error} errorCode={error.code} />}
+      {status !== 'error' && (
+        <Component
+          status={status}
+          url={url}
+          data={status === 'success' ? data : fakeData}
+          isFake={status !== 'success'}
+          {...rest}
+        />
+      )}
+    </>
+  )
+}
+export default StaticPageLoader
--- src/pages/Page.js
+++ src/pages/Page.js
@@ -1,4 +1,19 @@
 import { useLocation } from 'react-router-dom'
+import StaticPageLoader from '../components/StaticPageLoader'
+
+const PageContent = ({ data }) => {
+  if (!data) {
+    return null
+  }
+  const title = data.data.title?.rendered
+  const content = data.data.content?.rendered
+  return (
+    <div>
+      <h1>{title}</h1>
+      <section dangerouslySetInnerHTML={{ __html: content }}></section>
+    </div>
+  )
+}
 
 const Page = ({}) => {
   const location = useLocation()
@@ -7,10 +22,15 @@ const Page = ({}) => {
 
   return (
     <div
-      className='Page position-absolute'
+      className="Page position-absolute"
       style={{ marginTop: 200, opacity: location.pathname === '/cfp' ? 1 : 0 }}
     >
-      <h1>Call for papers</h1>
+      {location.pathname === '/cfp' && (
+        <StaticPageLoader
+          url={[process.env.REACT_APP_API_ROOT, process.env.REACT_APP_PAGES_CFP_URL].join('')}
+          Component={PageContent}
+        />
+      )}
     </div>
   )
 }
