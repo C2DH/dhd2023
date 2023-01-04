3254f454d12af7d64cfc771d06a70593f590112b
Daniele Guido <gui.daniele@gmail.com>
2023-01-04 10:42:25 +0100

    add repo info in footer

--- src/components/menu/MenuFooter.js
+++ src/components/menu/MenuFooter.js
@@ -6,11 +6,11 @@ const MenuFooter = () => {
   return (
     <div className="footer-menu-wrapper d-flex align-items-center">
       <p>© 2022</p>
-      <span className="footer-line mx-3"></span>
+      <span className="footer-line ms-3"></span>
       <ul className="d-flex align-content-flex-end">
         {dataFooterMenu.map(({ title, to, reloadDocument }, index) => {
           return (
-            <li key={index}>
+            <li key={index} className="ms-3">
               {reloadDocument ? (
                 <a href={to} target="_blank" rel="noreferrer">
                   {title}
@@ -21,6 +21,13 @@ const MenuFooter = () => {
             </li>
           )
         })}
+        <li className="ms-3">
+          <a
+            href={`${process.env.REACT_APP_GITHUB_REPO}/commit/${process.env.REACT_APP_GIT_REVISION}`}
+          >
+            v.&nbsp;{process.env.REACT_APP_GIT_REVISION}
+          </a>
+        </li>
       </ul>
     </div>
   )
